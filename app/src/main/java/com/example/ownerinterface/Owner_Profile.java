bbda15623c59025ee64dddce74aea8b50dde025e
cicekapM <ciceron.kapena-bantu-muetu@grenoble-inp.org>
2019-01-22 08:00:23 +0100

    Initial commit of the Owner Navigation Drawer

---
+++ app/src/main/java/com/example/ownerinterface/Owner_Profile.java
@@ -0,0 +1,29 @@
+package com.example.ownerinterface;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Owner_Profile extends Fragment {
+
+    // MENU 1
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.owner_fragment, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("PROFILE");
+    }
+}
